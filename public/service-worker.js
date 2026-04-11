97f0f1498c6cd4e83cd8ed0b14ee2c45afb5f1c2
nathanbrownlee008-web <nathanbrownlee008@gmail.com>
2026-04-11 01:42:58 +0100

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,9 @@
-self.addEventListener('install', ()=> self.skipWaiting());
+
+const CACHE = "tdt-scanner-v1";
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", () => {});
